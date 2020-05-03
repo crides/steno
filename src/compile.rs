84cbc836d3731cc6a655fee102d3607476a47164
crides <zhuhaoqing@live.cn>
2020-05-03 15:46:09 -0500

    Change format to HashMap

--- src/compile.rs
+++ src/compile.rs
@@ -2,22 +2,26 @@ use std::io::{self, prelude::*};
 
 use crate::dict::Dict;
 use crate::stroke::Stroke;
+use crate::hashmap::LPHashMap;
 
 struct IRNode {
-    node_num: u16,
-    level: u8,
+    node_num: u32,
     string: String,
-    children: Vec<(u32, u32)>,
+    children: LPHashMap,
 }
 
 impl IRNode {
-    fn new(node_num: u16, level: u8, string: String) -> Self {
+    fn new(node_num: u32, string: String) -> Self {
+        let children = LPHashMap::new(node_num as usize);
         Self {
-            node_num,
-            level,
+            node_num: children.total_size() as u32,
             string,
-            children: Vec::with_capacity(node_num as usize),
+            children,
         }
+    }
+
+    fn add_child(&mut self, input: u32, addr: u32) {
+        self.children.add_child(input, addr);
     }
 }
 
@@ -42,45 +46,45 @@ impl IR {
         self.cur_addr
     }
 
-    pub fn add_node(&mut self, node_num: u16, level: u8, string: String) {
-        self.cur_addr += 8 * node_num as u32 + string.len() as u32 + 4;
-        self.nodes.push(IRNode::new(node_num, level, string));
+    pub fn add_node(&mut self, node_num: u32, string: String) {
+        let len = string.len();
+        let new_node = IRNode::new(node_num, string);
+        self.cur_addr += 6 * new_node.children.total_size() as u32 + len as u32 + 4;
+        self.nodes.push(new_node);
     }
 
     pub fn from_dict(dict: Dict) -> Self {
         let mut ir = IR::new();
-        ir._from_dict(dict, 0);
+        assert_eq!(ir._from_dict(dict), 0);
         ir
     }
 
-    fn _from_dict(&mut self, dict: Dict, level: u8) -> u32 {
+    fn _from_dict(&mut self, dict: Dict) -> u32 {
         let addr = self.addr();
         let mut children: Vec<(Stroke, Dict)> = dict.children.into_iter().collect();
         children.sort_by(|a, b| a.0.cmp(&b.0));
         let cur_ind = self.len();
-        self.add_node(children.len() as u16, level, dict.entry.unwrap_or("".into()));
+        self.add_node(children.len() as u32, dict.entry.unwrap_or("".into()));
 
         for child in children.into_iter() {
-            let child_addr = self._from_dict(child.1, level + 1);
-            self.nodes[cur_ind].children.push((child.0.raw(), child_addr));
+            let child_addr = self._from_dict(child.1);
+            self.nodes[cur_ind].add_child(child.0.raw(), child_addr);
         }
+        let stats = self.nodes[cur_ind].children.stats();
+        println!("{} {} {}", stats.0, stats.1, stats.2);
         addr
     }
 
     pub fn write(&self, w: &mut dyn Write) -> io::Result<()> {
+        let mut bytes: Vec<u8> = Vec::new();
         for node in self.nodes.iter() {
-            let node_num = node.node_num;
+            bytes.clear();
             let string_len = node.string.len();
-            let mut bytes: Vec<u8> = Vec::new();
-            bytes.extend_from_slice(&node_num.to_le_bytes());
-            bytes.push(node.level);
+            bytes.extend_from_slice(&(node.children.total_size() as u32).to_le_bytes()[0..3]);
             bytes.push(string_len as u8);
             bytes.extend_from_slice(&node.string.clone().into_bytes());
-            for child in node.children.iter() {
-                bytes.extend_from_slice(&child.0.to_le_bytes());
-                bytes.extend_from_slice(&child.1.to_le_bytes());
-            }
-            w.write_all(&bytes).expect("write bytes");
+            w.write_all(&bytes)?;
+            node.children.write_all_to(w)?;
         }
         Ok(())
     }
