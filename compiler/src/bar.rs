4c393d6c6959efa660e1c024833f6a2e0982b350
crides <zhuhaoqing@live.cn>
2020-06-01 23:43:00 -0500

    Remove draw_delta

--- compiler/src/bar.rs
+++ compiler/src/bar.rs
@@ -5,7 +5,6 @@ pub fn progress_bar(len: usize, msg: &str) -> ProgressBar {
         .with_style(ProgressStyle::default_bar()
             .progress_chars("=> ")
             .template("{msg} [{wide_bar}] ETA: {eta} ({bytes}/{total_bytes})"));
-    bar.set_draw_delta(len as u64 / 200);
     bar.set_message(msg);
     bar
 }
