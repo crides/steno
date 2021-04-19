crides/steno

commit 4744a001d8c20f5c5adc3674a1a3daf0696024aa

[firmware] Refractor
- Made term naming more coherent. Previously `entry` has ambiguous
  meaning
  * KVpair: the valid part of a allocated block, which a bucket points
            to
  * Entry: the part of a KVpair after the strokes
- Abstracts the stroage (currently flash), UI/display, and keyboard
  hooks' interfaces, to facilitate easier porting to other platforms
- `const` and `static`-ified the core
- Some other small fixes

qmk/impl/qmk/flash.c
@@ -1,0 +1,164 @@
+// Functions for interacting with SPI flash
+#include <string.h>
+#include "store.h"
+#include "steno.h"
+#include "spi.h"
+
+#ifdef STENO_DEBUG_FLASH
+uint8_t flash_debug_enable = 0;
+#endif
+
+// Program page size
+#define FLASH_PP_SIZE 256
+#define FLASH_ERASED_BYTE 0xFF
+
+void store_init(void) {
+    spi_init();
+}
+
+void store_read(const uint32_t offset, uint8_t *const buf, const uint8_t len) {
+#ifdef STENO_DEBUG_FLASH
+    if (flash_debug_enable) {
+        steno_debug_ln("flash_read(# 0x%02X @ 0x%06lX)", len, offset);
+    }
+#endif
+    select_card();
+    spi_send_byte(0x03);    // read 
+    spi_send_addr(offset);
+    for (uint8_t i = 0; i < len; i ++) {
+        buf[i] = spi_recv_byte();
+    }
+    unselect_card();
+}
+
+// Read a program page into buffer
+static void flash_read_page(uint32_t addr, uint8_t *buf) {
+#ifdef STENO_DEBUG_FLASH
+    if (flash_debug_enable) {
+        steno_debug_ln("flash_read_page(@ 0x%06lX)", addr);
+    }
+#endif
+    select_card();
+    spi_send_byte(0x03);    // read 
+    spi_send_addr(addr);
+    for (uint8_t i = 0; i < 255; i ++) {
+        buf[i] = spi_recv_byte();
+    }
+    buf[255] = spi_recv_byte();
+    unselect_card();
+}
+
+void flash_flush(void) {
+    select_card();
+    while (1) {
+        spi_send_byte(0x05);    // read status reg
+        const uint8_t status = spi_recv_byte();
+        if (!(status & 0x01)) {
+            break;
+        }
+    }
+    unselect_card();
+}
+
+void store_flush(void) {
+    flash_flush();
+}
+
+static void flash_prep_write(void) {
+    flash_flush();
+    select_card();
+    spi_send_byte(0x06);    // write enable
+    unselect_card();
+}
+
+static void flash_write(const uint32_t addr, const uint8_t *const buf, const uint8_t len) {
+#ifdef STENO_DEBUG_FLASH
+    if (flash_debug_enable) {
+        steno_debug_ln("flash_write(# 0x%02X @ 0x%06lX)", len, addr);
+    }
+#endif
+    flash_prep_write();
+    select_card();
+    spi_send_byte(0x02);    // program
+    spi_send_addr(addr);
+    for (uint8_t i = 0; i < len; i ++) {
+        spi_send_byte(buf[i]);
+    }
+    unselect_card();
+}
+
+void store_write_direct(const uint32_t offset, const uint8_t *const buf, const uint8_t len) {
+    flash_write(offset, buf, len);
+}
+
+static void flash_write_page(const uint32_t addr, const uint8_t *const buf) {
+#ifdef STENO_DEBUG_FLASH
+    if (flash_debug_enable) {
+        steno_debug_ln("flash_write_page(@ 0x%06lX)", addr);
+    }
+#endif
+    flash_prep_write();
+    select_card();
+    spi_send_byte(0x02);    // program
+    spi_send_addr(addr);
+    for (uint8_t i = 0; i < 255; i ++) {
+        spi_send_byte(buf[i]);
+    }
+    spi_send_byte(buf[255]);
+    unselect_card();
+}
+
+static void flash_erase_4k(const uint32_t addr) {
+#ifdef STENO_DEBUG_FLASH
+    if (flash_debug_enable) {
+        steno_debug_ln("flash_erase_4k(@ 0x%06lX)", addr);
+    }
+#endif
+    flash_prep_write();
+    select_card();
+    spi_send_byte(0x20);
+    spi_send_addr(addr);
+    unselect_card();
+}
+
+void store_erase_partial(const uint32_t offset, const uint8_t len) {
+    uint8_t page_buffer[FLASH_PP_SIZE];
+    const uint32_t block_addr = offset & 0xFFF000; // Alighed to 4k, smallest Erase Unit
+    flash_erase_4k(SCRATCH_START);
+
+    const uint32_t page_addr = offset & 0xFFFF00; // Aligned to 256 (PP_SIZE)
+    for (uint32_t addr = block_addr, scratch_addr = SCRATCH_START; addr < block_addr + 0x1000; addr += FLASH_PP_SIZE) {
+        flash_flush();
+        flash_read_page(addr, page_buffer);
+        if (page_addr == addr) {
+            const uint8_t page_offset = offset & 0xFF;
+            memset(page_buffer + page_offset, FLASH_ERASED_BYTE, len);
+#ifdef STENO_DEBUG_DICTED
+            steno_debug_ln("cleared: offset: %02X, len: %02X", page_offset, len);
+#endif
+        }
+        flash_write_page(scratch_addr, page_buffer);
+        scratch_addr += FLASH_PP_SIZE;
+    }
+
+    flash_erase_4k(block_addr);
+    for (uint32_t addr = block_addr, scratch_addr = SCRATCH_START; addr < block_addr + 0x1000; addr += FLASH_PP_SIZE) {
+        flash_flush();
+        flash_read_page(scratch_addr, page_buffer);
+        flash_write_page(addr, page_buffer);
+        scratch_addr += FLASH_PP_SIZE;
+    }
+    flash_flush();
+}
+
+void store_rewrite_start(void) {
+    flash_prep_write();
+    select_card();
+    // Device erase
+    spi_send_byte(0xC7);
+    unselect_card();
+}
+
+void store_rewrite_write(const uint32_t offset, const uint8_t *const buf) {
+    flash_write(offset, buf, STORE_REWRITE_PACKET_SIZE);
+}

qmk/scsi.c
@@ -37,9 +37,8 @@
 
 #define INCLUDE_FROM_SCSI_C
 #include "scsi.h"
-#include "flash.h"
+#include "store.h"
 #include "stroke.h"
-#include "lcd.h"
 #include "dict_editing.h"
 
 static uint8_t packet_buf[EPSIZE];
@@ -144,31 +143,29 @@ void scsi_write(USB_ClassInfo_MS_Device_t *const msc_interface_info, const uint3
         }
     }
     for ( ; blocks > 0; blocks --) {
-        Endpoint_Read_Stream_LE(packet_buf, EPSIZE, NULL);
         uint8_t _header[32];
-        memcpy(_header, packet_buf, 32);
+        Endpoint_Read_Stream_LE(_header, 32, NULL);
+        const uint32_t *const header = (uint32_t *) _header;
+        const bool valid_header = (header[0] == UF2_MAGIC0 && header[1] == UF2_MAGIC1 && ((uint32_t *) packet_buf)[15] == UF2_MAGIC_END
+                && (header[2] & UF2_FLAG_FAMILYID) && (!(header[2] & UF2_FLAG_NOFLASH))
+                && ((header[3] & 0xFF) == 0) && header[4] == 256 && header[7] == UF2_FAMILY_ID);
         for (uint8_t i = 0; i < 4; i++) {
-            Endpoint_Read_Stream_LE(page_buffer + i * EPSIZE, EPSIZE, NULL);
-        }
-        for (uint8_t i = 0; i < 3; i++) {
             Endpoint_Read_Stream_LE(packet_buf, EPSIZE, NULL);
+            if (valid_header) {
+                if (header[5] == 0 && i == 0) {
+                    steno_error_ln("erase");
+                    store_rewrite_start();
+                    steno_error_ln("flash");
+                }
+                store_rewrite_write(header[3], packet_buf);
+            }
         }
+        Endpoint_Discard_Stream(512 - 256 - 32, NULL);
         if (msc_interface_info->State.IsMassStoreReset) {
             steno_error_ln("reset");
             return;
         }
 
-        uint32_t *header = (uint32_t *) _header;
-        if (header[0] == UF2_MAGIC0 && header[1] == UF2_MAGIC1 && ((uint32_t *) packet_buf)[15] == UF2_MAGIC_END
-                && (header[2] & UF2_FLAG_FAMILYID) && (!(header[2] & UF2_FLAG_NOFLASH))
-                && ((header[3] & 0xFF) == 0) && header[4] == 256 && header[7] == UF2_FAMILY_ID) {
-            if (header[5] == 0) {
-                steno_error_ln("erase");
-                flash_erase_device();
-                steno_error_ln("flash");
-            }
-            flash_write_page(header[3], page_buffer);
-        }
         if (!(Endpoint_IsReadWriteAllowed())) {
             Endpoint_ClearOUT();
         }
@@ -203,8 +200,8 @@ void scsi_read(USB_ClassInfo_MS_Device_t *const msc_interface_info, const uint32
  *  and capabilities to the host.
  */
 static bool scsi_inquiry(USB_ClassInfo_MS_Device_t *const msc_interface_info) {
-    uint16_t AllocationLength = SwapEndian_16(*(uint16_t *) &msc_interface_info->State.CommandBlock.SCSICommandData[3]);
-    uint16_t BytesTransferred = MIN(AllocationLength, sizeof(inquiry_data));
+    const uint16_t AllocationLength = SwapEndian_16(*(uint16_t *) &msc_interface_info->State.CommandBlock.SCSICommandData[3]);
+    const uint16_t BytesTransferred = MIN(AllocationLength, sizeof(inquiry_data));
 
     /* Only the standard INQUIRY data is supported, check if any optional INQUIRY bits set */
     if ((msc_interface_info->State.CommandBlock.SCSICommandData[1] & ((1 << 0) | (1 << 1))) ||
@@ -233,8 +230,8 @@ static bool scsi_inquiry(USB_ClassInfo_MS_Device_t *const msc_interface_info) {
  * failed to complete.
  */
 static bool scsi_request_sense(USB_ClassInfo_MS_Device_t *const msc_interface_info) {
-    uint8_t AllocationLength = msc_interface_info->State.CommandBlock.SCSICommandData[4];
-    uint8_t BytesTransferred = MIN(AllocationLength, sizeof(SenseData));
+    const uint8_t AllocationLength = msc_interface_info->State.CommandBlock.SCSICommandData[4];
+    const uint8_t BytesTransferred = MIN(AllocationLength, sizeof(SenseData));
 
     Endpoint_Write_Stream_LE(&SenseData, BytesTransferred, NULL);
     Endpoint_Null_Stream((AllocationLength - BytesTransferred), NULL);
@@ -250,8 +247,8 @@ static bool scsi_request_sense(USB_ClassInfo_MS_Device_t *const msc_interface_in
  * device's capacity on the selected Logical Unit (drive), as a number of OS-sized blocks.
  */
 static bool scsi_read_capacity_10(USB_ClassInfo_MS_Device_t *const msc_interface_info) {
-    uint32_t LastBlockAddressInLUN = FS_BLOCKS - 1;
-    uint32_t MediaBlockSize = BLOCK_SIZE;
+    const uint32_t LastBlockAddressInLUN = FS_BLOCKS - 1;
+    const uint32_t MediaBlockSize = BLOCK_SIZE;
 
     Endpoint_Write_Stream_BE(&LastBlockAddressInLUN, sizeof(LastBlockAddressInLUN), NULL);
     Endpoint_Write_Stream_BE(&MediaBlockSize, sizeof(MediaBlockSize), NULL);

qmk/flash.c
@@ -1,165 +1,0 @@
-// Functions for interacting with SPI flash
-#include <string.h>
-#include "flash.h"
-#include "steno.h"
-#include "spi.h"
-
-#ifdef STENO_DEBUG_FLASH
-uint8_t flash_debug_enable = 0;
-#endif
-
-void flash_init(void) {
-    // Assume SPI inited
-}
-
-void flash_read(uint32_t addr, uint8_t *buf, uint8_t len) {
-#ifdef STENO_DEBUG_FLASH
-    if (flash_debug_enable) {
-        steno_debug_ln("flash_read(# 0x%02X @ 0x%06lX)", len, addr);
-    }
-#endif
-    select_card();
-    spi_send_byte(0x03);    // read 
-    spi_send_addr(addr);
-    for (uint8_t i = 0; i < len; i ++) {
-        buf[i] = spi_recv_byte();
-    }
-    unselect_card();
-}
-
-// Read a program page into buffer
-void flash_read_page(uint32_t addr, uint8_t *buf) {
-#ifdef STENO_DEBUG_FLASH
-    if (flash_debug_enable) {
-        steno_debug_ln("flash_read_page(@ 0x%06lX)", addr);
-    }
-#endif
-    select_card();
-    spi_send_byte(0x03);    // read 
-    spi_send_addr(addr);
-    for (uint8_t i = 0; i < 255; i ++) {
-        buf[i] = spi_recv_byte();
-    }
-    buf[255] = spi_recv_byte();
-    unselect_card();
-}
-
-void flash_flush(void) {
-    select_card();
-    while (1) {
-        spi_send_byte(0x05);    // read status reg
-        uint8_t status = spi_recv_byte();
-        if (!(status & 0x01)) {
-            break;
-        }
-    }
-    unselect_card();
-}
-
-void flash_prep_write(void) {
-    flash_flush();
-    select_card();
-    spi_send_byte(0x06);    // write enable
-    unselect_card();
-}
-
-void flash_write(uint32_t addr, uint8_t *buf, uint8_t len) {
-#ifdef STENO_DEBUG_FLASH
-    if (flash_debug_enable) {
-        steno_debug_ln("flash_write(# 0x%02X @ 0x%06lX)", len, addr);
-    }
-#endif
-    flash_prep_write();
-    select_card();
-    spi_send_byte(0x02);    // program
-    spi_send_addr(addr);
-    for (uint8_t i = 0; i < len; i ++) {
-        spi_send_byte(buf[i]);
-    }
-    unselect_card();
-}
-
-void flash_write_page(uint32_t addr, uint8_t *buf) {
-#ifdef STENO_DEBUG_FLASH
-    if (flash_debug_enable) {
-        steno_debug_ln("flash_write_page(@ 0x%06lX)", addr);
-    }
-#endif
-    flash_prep_write();
-    select_card();
-    spi_send_byte(0x02);    // program
-    spi_send_addr(addr);
-    for (uint8_t i = 0; i < 255; i ++) {
-        spi_send_byte(buf[i]);
-    }
-    spi_send_byte(buf[255]);
-    unselect_card();
-}
-
-void flash_erase_64k(uint32_t addr) {
-    flash_prep_write();
-    select_card();
-    spi_send_byte(0xD8);
-    spi_send_addr(addr);
-    unselect_card();
-}
-
-void flash_erase_4k(uint32_t addr) {
-#ifdef STENO_DEBUG_FLASH
-    if (flash_debug_enable) {
-        steno_debug_ln("flash_erase_4k(@ 0x%06lX)", addr);
-    }
-#endif
-    flash_prep_write();
-    select_card();
-    spi_send_byte(0x20);
-    spi_send_addr(addr);
-    unselect_card();
-}
-
-void flash_erase_device(void) {
-    flash_prep_write();
-    select_card();
-    spi_send_byte(0xC7);
-    unselect_card();
-}
-
-uint64_t flash_check_crc_range(uint32_t start, uint32_t end, uint64_t crc) {
-    select_card();
-    spi_send_byte(0x9B);
-    spi_send_byte(0x27);
-    spi_send_byte(0xFE);
-    for (uint8_t i = 0; i < 8; i ++) {
-        spi_send_byte((crc >> (i * 8)) & 0xFF);
-    }
-    spi_send_byte(start & 0xFF);
-    spi_send_byte((start >> 8) & 0xFF);
-    spi_send_byte((start >> 16) & 0xFF);
-    spi_send_byte((start >> 24) & 0xFF);
-    spi_send_byte(end & 0xFF);
-    spi_send_byte((end >> 8) & 0xFF);
-    spi_send_byte((end >> 16) & 0xFF);
-    spi_send_byte((end >> 24) & 0xFF);
-    unselect_card();
-    _delay_ms(((end - start) >> 20) * 3);    // Assuming 3ms per 1MB block; typ 1.3ms
-    select_card();
-    spi_send_byte(0x70);        // Flag status register
-    uint8_t flag = spi_recv_byte();
-    unselect_card();
-    if (flag & 0x10) {
-        steno_error_ln("CRC check error");
-        select_card();
-        spi_send_byte(0x96);
-        for (uint8_t i = 0; i < 8; i ++) {
-            spi_recv_byte();
-        }
-        uint64_t actual_crc;
-        for (uint8_t i = 0; i < 8; i ++) {
-            actual_crc |= (uint64_t) spi_recv_byte() << (i * 8);
-        }
-        return actual_crc;
-    } else {
-        steno_debug_ln("CRC check OK");
-        return 0;
-    }
-}

commit 92aa0bbbd62aeb63e430ae3065be0d212717aa34

[firmware] MSD flashing fix
Fix incorrect UF2 format checking introduced in 4744a00

qmk/impl/qmk/flash.c
@@ -160,5 +160,5 @@ void store_rewrite_start(void) {
 }
 
 void store_rewrite_write(const uint32_t offset, const uint8_t *const buf) {
-    flash_write(offset, buf, STORE_REWRITE_PACKET_SIZE);
+    flash_write_page(offset, buf);
 }

qmk/scsi.c
@@ -144,23 +144,24 @@ void scsi_write(USB_ClassInfo_MS_Device_t *const msc_interface_info, const uint3
     }
     for ( ; blocks > 0; blocks --) {
         uint8_t _header[32];
+        uint8_t data_buf[256];
         Endpoint_Read_Stream_LE(_header, 32, NULL);
         const uint32_t *const header = (uint32_t *) _header;
-        const bool valid_header = (header[0] == UF2_MAGIC0 && header[1] == UF2_MAGIC1 && ((uint32_t *) packet_buf)[15] == UF2_MAGIC_END
+        Endpoint_Read_Stream_LE(data_buf, sizeof(data_buf), NULL);
+        Endpoint_Discard_Stream(512 - 256 - 32 - sizeof(uint32_t), NULL);
+        uint32_t last_word;
+        Endpoint_Read_Stream_LE(&last_word, sizeof(uint32_t), NULL);
+        const bool valid_header = (header[0] == UF2_MAGIC0 && header[1] == UF2_MAGIC1 && last_word == UF2_MAGIC_END
                 && (header[2] & UF2_FLAG_FAMILYID) && (!(header[2] & UF2_FLAG_NOFLASH))
                 && ((header[3] & 0xFF) == 0) && header[4] == 256 && header[7] == UF2_FAMILY_ID);
-        for (uint8_t i = 0; i < 4; i++) {
-            Endpoint_Read_Stream_LE(packet_buf, EPSIZE, NULL);
-            if (valid_header) {
-                if (header[5] == 0 && i == 0) {
-                    steno_error_ln("erase");
-                    store_rewrite_start();
-                    steno_error_ln("flash");
-                }
-                store_rewrite_write(header[3], packet_buf);
+        if (valid_header) {
+            if (header[5] == 0) {
+                steno_error_ln("erase");
+                store_rewrite_start();
+                steno_error_ln("flash");
             }
+            store_rewrite_write(header[3], data_buf);
         }
-        Endpoint_Discard_Stream(512 - 256 - 32, NULL);
         if (msc_interface_info->State.IsMassStoreReset) {
             steno_error_ln("reset");
             return;