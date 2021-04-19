crides/steno

commit 731a5c0116b16f9df49ad9aae6fa3381741bec5e

[firmware] Add config options for features
... to disable dictionary editing, screens, and/or dictionary loading
thru mass storage device

qmk/hist.c
@@ -8,15 +8,20 @@
 #include "steno.h"
 #include "store.h"
 #include "process_keycode/process_unicode_common.h"
+#ifndef STENO_READONLY
 #include "dict_editing.h"
+#endif
 #include "orthography.h"
+#ifndef STENO_NOUI
 #include "disp.h"
+#endif
 
 static history_t history[HIST_SIZE];
 
 extern char last_trans[128];
 extern uint8_t last_trans_size;
 
+#ifndef STENO_READONLY
 static void dict_edit_puts(const char *const str) {
     const uint16_t len = strlen(str);
     if (len + (uint16_t) entry_buf_len > 255) {
@@ -26,8 +31,10 @@ static void dict_edit_puts(const char *const str) {
     entry_buf_len += len;
     disp_edit_trans_handle_str(str);
 }
+#endif
 
 static void steno_back(const uint8_t len) {
+#ifndef STENO_READONLY
     if (editing_state != ED_IDLE) {
         if (entry_buf_len > len) {
             disp_edit_trans_back(len);
@@ -36,7 +43,9 @@ static void steno_back(const uint8_t len) {
             disp_edit_trans_back(entry_buf_len);
             entry_buf_len = 0;
         }
-    } else {
+    } else
+#endif
+    {
         for (uint8_t i = 0; i < len; i ++) {
             tap_code(KC_BSPC);
         }
@@ -44,12 +53,15 @@ static void steno_back(const uint8_t len) {
 }
 
 static void steno_send_char(const char c) {
+#ifndef STENO_READONLY
     if (editing_state != ED_IDLE) {
         if (entry_buf_len < 255) {
             entry_buf[entry_buf_len ++] = c;
             disp_edit_trans_handle_char(c);
         }
-    } else {
+    } else
+#endif
+    {
         if (last_trans_size < 128) {
             last_trans[last_trans_size++] = c;
         }
@@ -77,10 +89,13 @@ static uint8_t steno_send_unicode(const uint32_t u) {
         snprintf(buf, 16, "\\U%06lX", u);
         len = 8;
     }
+#ifndef STENO_READONLY
     if (editing_state != ED_IDLE) {
         dict_edit_puts(buf);
         return len;
-    } else {
+    } else
+#endif
+    {
         if (last_trans_size + len < 128) {
             for (uint8_t i = 0; i < len; i ++) {
                 last_trans[last_trans_size++] = buf[i];
@@ -95,12 +110,14 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
 #ifdef STENO_DEBUG_HIST
     steno_debug("keys(%u):", len);
 #endif
+    uint8_t mods = 0;
+#ifndef STENO_READONLY
     char buf[16];
     uint8_t output_len = 3;
-    uint8_t mods = 0;
     if (editing_state != ED_IDLE) {
         dict_edit_puts("\\k[");
     }
+#endif
     for (uint8_t i = 0; i < len; i++) {
         if ((keycodes[i] & 0xFC) == 0xE0) {
             const uint8_t mod = keycodes[i] & 0x03;
@@ -113,21 +130,27 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
             }
             const uint8_t mod_mask = 1 << mod;
             if (mods & mod_mask) {
+#ifndef STENO_READONLY
                 if (editing_state != ED_IDLE) {
                     dict_edit_puts(")");
                     output_len += 1;
-                } else {
+                } else
+#endif
+                {
                     unregister_code(keycodes[i]);
                 }
 #ifdef STENO_DEBUG_HIST
                 steno_debug(" %c", mod_char);
 #endif
             } else {
+#ifndef STENO_READONLY
                 if (editing_state != ED_IDLE) {
                     snprintf(buf, 16, "\\%c(", mod_char);
                     dict_edit_puts(buf);
                     output_len += 3;
-                } else {
+                } else
+#endif
+                {
                     register_code(keycodes[i]);
                 }
 #ifdef STENO_DEBUG_HIST
@@ -139,11 +162,14 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
 #ifdef STENO_DEBUG_HIST
             steno_debug(" %02X", keycodes[i]);
 #endif
+#ifndef STENO_READONLY
             if (editing_state != ED_IDLE) {
                 snprintf(buf, 16, " %02X", keycodes[i]);
                 dict_edit_puts(buf);
                 output_len += 3;
-            } else {
+            } else
+#endif
+            {
                 tap_code(keycodes[i]);
             }
         }
@@ -151,10 +177,13 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
 #ifdef STENO_DEBUG_HIST
     steno_debug("\n");
 #endif
+#ifndef STENO_READONLY
     if (editing_state != ED_IDLE) {
         dict_edit_puts("]");
         return output_len + 1;
-    } else {
+    } else
+#endif
+    {
         return 0;
     }
 }
@@ -399,6 +428,7 @@ state_t process_output(const uint8_t h_ind) {
                 new_state.cap = CAPS_LOWER;
                 break;
 
+#ifndef STENO_READONLY
             case 16: // add translation
                 if (editing_state != ED_IDLE) {
                     dict_edit_puts("{add_trans}");
@@ -425,6 +455,7 @@ state_t process_output(const uint8_t h_ind) {
                     dicted_remove_prompt_strokes();
                 }
                 break;
+#endif
 
             default:
                 steno_error_ln("\nInvalid cmd: %X", entry[i]);

qmk/steno.c
@@ -3,11 +3,13 @@
 #include "steno.h"
 #include "keymap_steno.h"
 #include "store.h"
-#include "disp.h"
 #include "hist.h"
+#ifndef STENO_READONLY
 #include "dict_editing.h"
-#include <LUFA/Drivers/USB/USB.h>
-#include "scsi.h"
+#endif
+#ifndef STENO_NOUI
+#include "disp.h"
+#endif
 
 bool flashing = false;
 char last_stroke[24];
@@ -46,16 +48,22 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
     stroke_to_string(stroke, last_stroke, NULL);
     /* #endif */
 
+#ifndef STENO_READONLY
     if (handle_dict_editing(stroke)) {
         return;
     }
+#endif
 
     if (stroke == 0x1000) { // Asterisk
         hist_ind = HIST_LIMIT(hist_ind - 1);
         hist_undo(hist_ind);
+#ifndef STENO_READONLY
         if (editing_state == ED_IDLE) {
+#ifndef STENO_NOUI
             disp_tape_show_star();
+#endif
         }
+#endif
         return;
     }
 
@@ -78,7 +86,9 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
 #ifdef STENO_DEBUG_HIST
     steno_debug_ln("next %u: scg: %u%u%u", HIST_LIMIT(hist_ind + 1), new_state.space, new_state.cap, new_state.glue);
 #endif
+#ifndef STENO_READONLY
     if (editing_state == ED_IDLE) {
+#ifndef STENO_NOUI
         if (strokes_len > 0) {
             const uint32_t first_stroke = *((uint32_t *) kvpair_buf);
             disp_tape_show_strokes_start();
@@ -94,7 +104,9 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
         } else {
             disp_tape_show_raw_stroke(hist->stroke);
         }
+#endif
     }
+#endif
     if (hist->len) {
 #ifdef STENO_DEBUG_HIST
         steno_debug_ln("hist %u:", hist_ind);
@@ -125,5 +137,7 @@ void ebd_steno_init(void) {     // to avoid clashing with `steno_init` in QMK
     steno_set_mode(STENO_MODE_GEMINI);
     hist_get(0)->state.cap = CAPS_CAP;
     store_init();
+#ifndef STENO_NOUI
     disp_init();
+#endif
 }

qmk/config.h
@@ -3,7 +3,6 @@
 
 #include "config_common.h"
 
-#define CUSTOM_STENO
 #define USE_SPI_FLASH
 
 #define STENO_DEBUG
@@ -21,9 +20,7 @@
 #define SERIAL_NUMBER   v2
 #define DESCRIPTION     28 key steno board
 
-#ifdef CUSTOM_STENO
 #undef VIRTSER_ENABLE
-#endif
 
 #define MATRIX_ROWS 5
 #define MATRIX_COLS 6

qmk/dict_editing.c
@@ -6,7 +6,9 @@
 #include "hist.h"
 #include "dict_editing.h"
 #include "stroke.h"
+#ifndef STENO_NOUI
 #include "disp.h"
+#endif
 
 editing_state_t editing_state = ED_IDLE;
 // Buffer for a page; used for unparsed/formatted entry and copying between pages

qmk/disp.h
@@ -1,3 +1,14 @@
+void disp_init(void);
+
+void disp_tape_show_raw_stroke(const uint32_t stroke);
+void disp_tape_show_star(void);
+void disp_tape_show_stroke(const uint32_t stroke);
+void disp_tape_show_stroke_sep(void);
+void disp_tape_show_strokes_done(void);
+void disp_tape_show_strokes_start(void);
+void disp_tape_show_trans(const char *const trans);
+
+#ifndef STENO_READONLY
 void disp_edit_conf_entry(const char *const s);
 void disp_edit_done(void);
 void disp_edit_prompt_strokes(void);
@@ -5,8 +16,6 @@ void disp_edit_prompt_trans(void);
 void disp_edit_trans_back(const uint8_t len);
 void disp_edit_trans_handle_char(const char c);
 void disp_edit_trans_handle_str(const char *const s);
-
-void disp_init(void);
 
 void disp_add_done(void);
 void disp_prompt_add_stroke(void);
@@ -23,11 +32,4 @@ void disp_unshow_error(void);
 
 void disp_stroke_edit_add(const uint32_t stroke, const uint8_t num_strokes);
 void disp_stroke_edit_remove(const uint32_t last, const uint8_t num_strokes);
-
-void disp_tape_show_raw_stroke(const uint32_t stroke);
-void disp_tape_show_star(void);
-void disp_tape_show_stroke(const uint32_t stroke);
-void disp_tape_show_stroke_sep(void);
-void disp_tape_show_strokes_done(void);
-void disp_tape_show_strokes_start(void);
-void disp_tape_show_trans(const char *const trans);
+#endif

qmk/rules.mk
@@ -1,3 +1,10 @@
+# Readonly dictionary. Dictionary editing is disabled
+STENO_READONLY = no
+# No display available for UI. Implies STENO_READONLY
+STENO_NOUI = no
+# No mass storage device enabled
+STENO_NOMSD = no
+
 MCU = atmega32u4
 F_CPU = 16000000
 ARCH = AVR8
@@ -5,8 +12,28 @@ F_USB = $(F_CPU)
 BOOTLOADER = atmel-dfu
 EXTRAKEY_ENABLE = no
 
-SRC += hist.c stroke.c dict_editing.c scsi.c ghostfat.c freemap.c orthography.c
-SRC += impl/qmk/hooks.c impl/qmk/disp.c impl/qmk/lcd.c impl/qmk/lcd_font.c impl/qmk/spi.c impl/qmk/flash.c
+SRC += hist.c stroke.c orthography.c
+SRC += impl/qmk/hooks.c impl/qmk/spi.c impl/qmk/flash.c
+ifeq ($(STENO_NOUI),yes)
+	STENO_READONLY = yes
+	CFLAGS += -DSTENO_NOUI
+else
+	SRC += impl/qmk/disp.c impl/qmk/lcd.c impl/qmk/lcd_font.c
+endif
+
+ifeq ($(STENO_READONLY),yes)
+	CFLAGS += -DSTENO_READONLY
+else
+	SRC += dict_editing.c freemap.c
+endif
+
+ifeq ($(STENO_NOMSD),yes)
+	CFLAGS += -DSTENO_NOMSD
+	MSC_ENABLE = no
+else
+	SRC += scsi.c ghostfat.c
+	MSC_ENABLE = yes
+endif
 
 MOUSEKEY_ENABLE = no
 VIRTSER_ENABLE = no
@@ -16,7 +43,6 @@ STENO_ENABLE = yes
 COMBO_ENABLE = no
 OLED_DRIVER_ENABLE = no
 UNICODE_ENABLE = yes
-MSC_ENABLE = yes
 
 EXTRAFLAGS += -flto
 LTO_ENABLE = yes

commit 93afd74fd3774c3f783b3c77b59cb54ed591a22b

[firmware] display config fix
issue introduced in 731a5c0

qmk/hist.c
@@ -121,6 +121,7 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
     for (uint8_t i = 0; i < len; i++) {
         if ((keycodes[i] & 0xFC) == 0xE0) {
             const uint8_t mod = keycodes[i] & 0x03;
+#if defined(STENO_DEBUG_HIST) || !defined(STENO_READONLY)
             uint8_t mod_char;
             switch (mod) {
             case 0: mod_char = 'c'; break;
@@ -128,6 +129,7 @@ static uint8_t steno_send_keycodes(const uint8_t *keycodes, const uint8_t len) {
             case 2: mod_char = 'a'; break;
             case 3: mod_char = 'g'; break;
             }
+#endif
             const uint8_t mod_mask = 1 << mod;
             if (mods & mod_mask) {
 #ifndef STENO_READONLY

qmk/impl/qmk/disp.c
@@ -132,6 +132,7 @@ void disp_tape_show_star(void) {
 }
 
 void disp_tape_show_raw_stroke(const uint32_t stroke) {
+    disp_clear();
     select_lcd();
     lcd_pos(0, 0);
     unselect_lcd();

qmk/steno.c
@@ -57,11 +57,12 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
     if (stroke == STENO_STAR) {
         hist_ind = HIST_LIMIT(hist_ind - 1);
         hist_undo(hist_ind);
-#ifndef STENO_READONLY
-        if (editing_state == ED_IDLE) {
 #ifndef STENO_NOUI
-            disp_tape_show_star();
+#ifndef STENO_READONLY
+        if (editing_state == ED_IDLE)
 #endif
+        {
+            disp_tape_show_star();
         }
 #endif
         return;
@@ -86,9 +87,11 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
 #ifdef STENO_DEBUG_HIST
     steno_debug_ln("next %u: scg: %u%u%u", HIST_LIMIT(hist_ind + 1), new_state.space, new_state.cap, new_state.glue);
 #endif
-#ifndef STENO_READONLY
-    if (editing_state == ED_IDLE) {
 #ifndef STENO_NOUI
+#ifndef STENO_READONLY
+    if (editing_state == ED_IDLE)
+#endif
+    {
         if (strokes_len > 0) {
             disp_tape_show_strokes(kvpair_buf, strokes_len);
             last_trans[last_trans_size] = 0;
@@ -96,7 +99,6 @@ void _ebd_steno_process_stroke(const uint32_t stroke) {
         } else {
             disp_tape_show_raw_stroke(hist->stroke);
         }
-#endif
     }
 #endif
     if (hist->len) {