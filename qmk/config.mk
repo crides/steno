df1494c4cc12515dfe0fdf418f319bb5c5491890
crides <zhuhaoqing@live.cn>
2021-04-30 18:32:54 -0500

    [firmware] Config refractor
    Concentrate all the configs into `config.mk`, and use `rules.mk` as
    config processing

---
+++ qmk/config.mk
@@ -0,0 +1,11 @@
+# Readonly dictionary. Dictionary editing is disabled
+STENO_READONLY = no
+# No display available for UI. Implies STENO_READONLY
+STENO_NOUI = no
+# No mass storage device enabled
+STENO_NOMSD = no
+# Graphical stroke display for demos
+STENO_STROKE_DISPLAY = no
+STENO_NOUNICODE = yes
+
+STENO_DEBUG = hist # stroke flash dicted
