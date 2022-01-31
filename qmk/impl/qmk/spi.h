e346afa6f7e0caa06d1f1cd5ef066d4b5cda1900
crides <zhuhaoqing@live.cn>
2022-01-30 23:44:34 -0600

    correct SPI CS

--- qmk/impl/qmk/spi.h
+++ qmk/impl/qmk/spi.h
@@ -6,10 +6,10 @@
 #define configure_pin_sck() DDRB |= _BV(DDB1)
 #define configure_pin_mosi() DDRB |= _BV(DDB2)
 #define configure_pin_miso() DDRB &= ~_BV(DDB3)
-#define configure_pin_ss() DDRD |= _BV(DDD5)
+#define configure_pin_ss() DDRC |= _BV(DDC6)
 
-#define select_card() PORTD &= ~_BV(PORTD5)
-#define unselect_card() PORTD |= _BV(PORTD5)
+#define select_card() PORTC &= ~_BV(PORTC6)
+#define unselect_card() PORTC |= _BV(PORTC6)
 
 void spi_init(void);
 void spi_send_byte(uint8_t);
